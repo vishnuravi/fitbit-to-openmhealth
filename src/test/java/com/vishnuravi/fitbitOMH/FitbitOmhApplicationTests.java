a733a5d37d49839b5f06bff3a83a45af4c2a0a0e
Vishnu Ravi <vishnu.ravi@gmail.com>
2021-07-14 09:15:55 -0400

    Add sanity check test

--- src/test/java/com/vishnuravi/fitbitOMH/FitbitOmhApplicationTests.java
+++ src/test/java/com/vishnuravi/fitbitOMH/FitbitOmhApplicationTests.java
@@ -1,13 +1,21 @@
 package com.vishnuravi.fitbitOMH;
 
+import com.vishnuravi.fitbitOMH.controller.HeartRateController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FitbitOmhApplicationTests {
 
+	@Autowired
+	private HeartRateController heartRateController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(heartRateController).isNotNull();
 	}
 
 }
